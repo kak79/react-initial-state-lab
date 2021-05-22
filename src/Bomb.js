4ca3a4afc1213ef0c4e48a704eb59d7343464611
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-05-22 11:53:29 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,12 @@
 // your Bomb code here!
+import React from "react"
+
+class Bomb extends React.Component {
+  render() {
+    return (
+      <p>hi</p>
+    );
+  }
+}
+
+export default Bomb;
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,21 @@
 // your ImageSlider code here!
+import React from "react"
+
+class ImageSlider extends React.Component {
+  render() {
+    
+    constructor() {
+      super()
+      this.state = {
+        currentSlideIndex: 0
+      }
+    }
+
+    return (
+      <p>I am on slide {this.state.currentSlideIndex}</p>
+    )
+
+  }
+}
+
+export default ImageSlider;
